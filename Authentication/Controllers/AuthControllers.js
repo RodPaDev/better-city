e28ba772a5b84cd22c48966993c80ccb87f9229c
Patrick Rodrigues <47316946+rodpa715@users.noreply.github.com>
2019-11-18 19:19:04 +0100

    add login controller

--- Authentication/Controllers/AuthControllers.js
+++ Authentication/Controllers/AuthControllers.js
@@ -18,7 +18,18 @@ async function register(req, res) {
   }
 }
 
+async function login(req, res) {
+  try {
+    const result = await users.login(req.body)
+    res.status(200).json(result)
+  } catch (error) {
+    res.status(500).json("Something went wrong, try again in a few minutes.");
+  }
+}
+
+
 module.exports = {
   getAllUsers,
-  register
+  register,
+  login
 };
