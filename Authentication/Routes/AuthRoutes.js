2df66a406ef651ef93351897fc327031029f511d
Patrick Rodrigues <47316946+rodpa715@users.noreply.github.com>
2019-11-18 18:16:49 +0100

    use hashPassword middleware

--- Authentication/Routes/AuthRoutes.js
+++ Authentication/Routes/AuthRoutes.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 const { getAllUsers, register } = require("../Controllers/AuthControllers");
 const { validateForm } = require("../Middleware/ValidateForm");
 const { uniqueContactInfo } = require("../Middleware/UniqueEmail");
+const { hashPassword } = require("../Middleware/hashPassword");
+
 
 router.get("/auth", getAllUsers);
-router.post("/auth", validateForm, uniqueContactInfo, register);
+router.post("/auth", validateForm, uniqueContactInfo, hashPassword, register);
 
 module.exports = router;
