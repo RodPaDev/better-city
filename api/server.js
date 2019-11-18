55184c048d5f09a830da88d3e2b704349b7c645c
Patrick Rodrigues <47316946+rodpa715@users.noreply.github.com>
2019-11-18 10:17:10 +0100

    add boilerplate

---
+++ api/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+server.get("/", (req, res) => {
+    res.send("<h1>Hello, I am running. Are you running?")
+})
+
+module.exports = server;
