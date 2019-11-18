7f98816bd8a45da4d90d9bb255e8c6416a97509c
Patrick Rodrigues <47316946+rodpa715@users.noreply.github.com>
2019-11-18 19:19:16 +0100

    add login router & rename endpoints

--- Authentication/Routes/AuthRoutes.js
+++ Authentication/Routes/AuthRoutes.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
-const { getAllUsers, register } = require("../Controllers/AuthControllers");
+const { getAllUsers, register, login } = require("../Controllers/AuthControllers");
 const { validateForm } = require("../Middleware/ValidateForm");
 const { uniqueContactInfo } = require("../Middleware/UniqueEmail");
 const { hashPassword } = require("../Middleware/hashPassword");
 
 
-router.get("/auth", getAllUsers);
-router.post("/auth", validateForm, uniqueContactInfo, hashPassword, register);
+router.get("/users", getAllUsers);
+router.post("/register", validateForm, uniqueContactInfo, hashPassword, register);
+router.post("/login", login);
+
 
 module.exports = router;
