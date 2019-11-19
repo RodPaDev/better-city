78a97df945e9c1c68e9307b821f37d7ca7b82c07
Patrick Rodrigues <47316946+rodpa715@users.noreply.github.com>
2019-11-19 17:32:25 +0100

    reformat

--- Authentication/Routes/AuthRoutes.js
+++ Authentication/Routes/AuthRoutes.js
@@ -1,13 +1,16 @@
 const router = require("express").Router();
+
+// Controllers
 const { getAllUsers, register, login } = require("../Controllers/AuthControllers");
+
+// Middlewares
 const { validateForm } = require("../Middleware/ValidateForm");
 const { uniqueContactInfo } = require("../Middleware/UniqueEmail");
 const { hashPassword } = require("../Middleware/hashPassword");
 
-
+// Authentication Routes
 router.get("/users", getAllUsers);
 router.post("/register", validateForm, uniqueContactInfo, hashPassword, register);
 router.post("/login", login);
-
 
 module.exports = router;
