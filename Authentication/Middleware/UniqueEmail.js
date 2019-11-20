43233f172dbc8d92649351bf1ea23e620412beda
Patrick Rodrigues <47316946+rodpa715@users.noreply.github.com>
2019-11-20 10:31:06 +0100

    change message on return and variables to be more readable

--- Authentication/Middleware/UniqueEmail.js
+++ Authentication/Middleware/UniqueEmail.js
@@ -2,9 +2,9 @@ const db = require("../../data/dbConfig");
 
 async function uniqueContactInfo(req, res, next) {
   try {
-    const emails = await db("users").select("email", "phone");
-    const isEmail = emails.filter(item => item.email === req.body.email);
-    const isPhone = emails.filter(item => item.phone === req.body.phone);
+    const users = await db("users").select("email", "phone");
+    const isEmail = users.filter(user => user.email === req.body.email);
+    const isPhone = users.filter(user => user.phone === req.body.phone);
     if (isEmail.length) {
       res.status(500).json(`${req.body.email} is already in use`);
     } else if (isPhone.length) {
@@ -13,7 +13,10 @@ async function uniqueContactInfo(req, res, next) {
       next();
     }
   } catch (error) {
-    res.status(500).json("Something went wrong, try again in a few minutes.");
+    res.status(500).json({
+      message: "Something went wrong, try again in a few minutes",
+      error
+    });
   }
 }
 
