372f26e7aed76c7321c40bba4305d59f96e8f1dc
Patrick Rodrigues <47316946+rodpa715@users.noreply.github.com>
2019-11-21 09:38:16 +0100

    add testing db

--- knexfile.js
+++ knexfile.js
@@ -15,6 +15,20 @@ module.exports = {
     seeds: { directory: "./data/seeds" }
   },
 
+  testing: {
+    client: "pg",
+    connection: {
+      port: process.env.DATABASE_TEST_PORT,
+      database: process.env.DATABASE_TEST,
+      user: process.env.DATABASE_TEST_USERNAME,
+      password: process.env.DATABASE_TEST_PASSWORD
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: { directory: "./data/seeds" }
+  },
+
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
